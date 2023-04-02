ace071a7964ce640c02619f5e89564884c18412a
Sara <saranatourcontact@gmail.com>
2023-04-03 01:10:17 +0300

    regestration page, html & css, addition of favicon

--- moods_project/login_with_regestration/urls.py
+++ moods_project/login_with_regestration/urls.py
@@ -3,5 +3,5 @@ from django.urls import path,include
 from . import views                    # import views from current file 
 urlpatterns = [
     path('',views.index),              # here i am refering to the method
-                                       # where the main route here is refering to the main page shown on load
+    path('register',views.show_registration_page), # where the main route here is refering to the main page shown on load
 ]
